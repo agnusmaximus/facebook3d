8ec90c36c878ebf0e8d2c76745d33d906a034e4f
Drenguin <drenguino@gmail.com>
2013-10-12 10:26:43 -0700

    Array of cubes?

--- src/cubes.js
+++ src/cubes.js
@@ -1,6 +1,7 @@
 var camera, scene, renderer;
-var mesh;
 var self_id;
+var friend_profile_pics = new Array();
+var cubesArr = new Array();
 
 function init() {
 
@@ -15,10 +16,31 @@ function init() {
 
 	scene = new THREE.Scene();
 
-	get_self(selfIDReceived);
+	//get_self(selfIDReceived);
+	get_all_friends(findFriends);
+
+	var size = 4;
+	for(var i = 0; i < size; i++) {
+		for(var j = 0; j < size; j++) {
+			var cube = new THREE.CubeGeometry(50,50,50);
+			var mesh = new THREE.Mesh(cube, new THREE.MeshBasicMaterial());
+			mesh.position = new THREE.Vector3((i-(size/2)-1)*50,(j-(size/2)-1,0)*50);
+			scene.add(mesh);
+		}
+	}
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
+}
+
+function findFriends(friends) {
+	for(var i = 0; i < friends.length; i++) {
+		get_friend_profile_pic(friends[i], profilePictureForFriend);
+	}
+}
+
+function profilePictureForFriend(picURL,data) {
+	friend_profile_pics.push(picURL);
 }
 
 function selfIDReceived(id) {
@@ -51,8 +73,10 @@ function animate() {
 
 	requestAnimationFrame( animate );
 
-	mesh.rotation.x += 0.005;
-	mesh.rotation.y += 0.01;
+	for(i in cubesArr) {
+		cubesArr[i].rotation.x += 0.005;
+		cubesArr[i].rotation.y += 0.01;
+	}
 
 	renderer.render( scene, camera );
 
