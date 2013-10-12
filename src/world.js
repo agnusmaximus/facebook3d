b4e0f27948dcb42ecea726a129eee3cb8aa01148
Paymon Payamipour <paymon.p@berkeley.edu>
2013-10-11 20:49:28 -0700

    core commit

--- src/world.js
+++ src/world.js
@@ -2,6 +2,8 @@ var camera, scene, renderer;
 var geometry, material, mesh;
 var controls,time = Date.now();
 
+var houseManager;
+
 var objects = [];
 
 var ray;
@@ -96,23 +98,87 @@ if ( havePointerLock ) {
 
 }
 
-init();
-animate();
+function House() {
+    
+}
+
+House.prototype.create = function(x, y, z) {
+    var backWall = new THREE.PlaneGeometry( 201, 60 );
+
+	backWallMesh = new THREE.Mesh(backWall, new THREE.MeshBasicMaterial( {color : 0xAAAAAA} ));
+	backWallMesh.position = new THREE.Vector3(x,y+30,z+100);
+    backWallMesh.material.side = THREE.DoubleSide;
+	scene.add( backWallMesh );
+
+
+	var frontWall = new THREE.PlaneGeometry( 201, 60 );
+
+	frontWallMesh = new THREE.Mesh(frontWall, new THREE.MeshBasicMaterial( {color : 0xAAAA00} ));
+	frontWallMesh.position = new THREE.Vector3(x,y+30,z-100);
+	frontWallMesh.rotation.x = 3.14;
+    frontWallMesh.material.side = THREE.DoubleSide;
+	scene.add( frontWallMesh );
+
+
+	var leftWall = new THREE.PlaneGeometry( 201, 60 );
+
+	leftWallMesh = new THREE.Mesh(leftWall, new THREE.MeshBasicMaterial( {color : 0xAA00AA} ));
+	leftWallMesh.position = new THREE.Vector3(x-100,y+30,z);
+	leftWallMesh.rotation.y = 1.57;
+    leftWallMesh.material.side = THREE.DoubleSide;
+	scene.add( leftWallMesh );
+
+
+	var rightWall = new THREE.PlaneGeometry( 201, 60 );
+
+	rightWallMesh = new THREE.Mesh(rightWall, new THREE.MeshBasicMaterial( {color : 0x00AAAA} ));
+	rightWallMesh.position = new THREE.Vector3(x+100,y+30,z);
+	rightWallMesh.rotation.y = -1.57;
+    rightWallMesh.material.side = THREE.DoubleSide;
+	scene.add( rightWallMesh );
+
+
+	var ceiling = new THREE.PlaneGeometry( 201, 201 );
+
+	ceilingMesh = new THREE.Mesh(ceiling, new THREE.MeshBasicMaterial( {color : 0x555555} ));
+	ceilingMesh.position = new THREE.Vector3(x,y+60,z);
+	ceilingMesh.rotation.x = 1.57;
+    ceilingMesh.material.side = THREE.DoubleSide;
+	scene.add( ceilingMesh );
+};
+
+
+function HouseManager() {
+    var housesLeft, housesRight;
+}
+
+HouseManager.prototype.init = function() {
+    housesLeft = new Array();
+    housesRight = new Array();
+    
+    for (var i = 0; i < 50; i++) {
+        housesLeft[i] = new House();
+        housesLeft[i].create(300, 0, -400 * i);
+        housesRight[i] = new House();
+        housesRight[i].create(-300, 0, -400 * i);
+    }
+};
+
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
 
 	scene = new THREE.Scene();
-	//scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+	scene.fog = new THREE.Fog( 0xffffff, 0, 2000 );
 
-	var light = new THREE.DirectionalLight( 0xffffff, 1.5 );
+	/*var light = new THREE.DirectionalLight( 0xffffff, 1.5 );
 	light.position.set( 1, 1, 1 );
 	scene.add( light );
 
 	var light = new THREE.DirectionalLight( 0xffffff, 0.75 );
 	light.position.set( -1, - 0.5, -1 );
-	scene.add( light );
+	scene.add( light );*/
 
 	controls = new THREE.PointerLockControls( camera );
 	scene.add( controls.getObject() );
@@ -125,63 +191,40 @@ function init() {
 
 	document.body.appendChild( renderer.domElement );
 
-	// ROOM
-
-	geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+	geometry = new THREE.PlaneGeometry( 20000000, 20000000, 100, 100 );
 	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
-	material = new THREE.MeshBasicMaterial( {color : 0xC0C0C0} );
+	material = new THREE.MeshBasicMaterial( {color : 0x006400} );
 
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
-
-	var backWall = new THREE.PlaneGeometry( 201, 60 );
-
-	backWallMesh = new THREE.Mesh(backWall, new THREE.MeshBasicMaterial( {color : 0xAAAAAA} ));
-	backWallMesh.position = new THREE.Vector3(0,30,-100);
-
-	scene.add( backWallMesh );
-
-
-	var frontWall = new THREE.PlaneGeometry( 201, 60 );
-
-	frontWallMesh = new THREE.Mesh(frontWall, new THREE.MeshBasicMaterial( {color : 0xAAAA00} ));
-	frontWallMesh.position = new THREE.Vector3(0,30,100);
-	frontWallMesh.rotation.x = 3.14;
-
-	scene.add( frontWallMesh );
-
-
-	var leftWall = new THREE.PlaneGeometry( 201, 60 );
-
-	leftWallMesh = new THREE.Mesh(leftWall, new THREE.MeshBasicMaterial( {color : 0xAA00AA} ));
-	leftWallMesh.position = new THREE.Vector3(-100,30,0);
-	leftWallMesh.rotation.y = 1.57;
-
-	scene.add( leftWallMesh );
-
-
-	var rightWall = new THREE.PlaneGeometry( 201, 60 );
-
-	rightWallMesh = new THREE.Mesh(rightWall, new THREE.MeshBasicMaterial( {color : 0x00AAAA} ));
-	rightWallMesh.position = new THREE.Vector3(100,30,0);
-	rightWallMesh.rotation.y = -1.57;
-
-	scene.add( rightWallMesh );
-
-
-	var ceiling = new THREE.PlaneGeometry( 201, 201 );
-
-	ceilingMesh = new THREE.Mesh(ceiling, new THREE.MeshBasicMaterial( {color : 0x555555} ));
-	ceilingMesh.position = new THREE.Vector3(0,60,0);
-	ceilingMesh.rotation.x = 1.57;
-
-	scene.add( ceilingMesh );
-
-	//
-
+    
+    houseManager = new HouseManager();
+    houseManager.init();
+    
+    // skybox
+    
+   /* var urlPrefix = "../data/";
+    var urls = [ urlPrefix + "1.png", urlPrefix + "2.png",
+        urlPrefix + "5.png", urlPrefix + "6.png",
+        urlPrefix + "3.png", urlPrefix + "4.png" ];
+    var textureCube = THREE.ImageUtils.loadTextureCube( urls );
+    
+    var shader = THREE.ShaderLib["cube"];
+    //var uniforms = THREE.UniformsUtils.clone( shader.uniforms );
+    shader.uniforms['tCube'].texture= textureCube;   // textureCube has been init before
+    var material = new THREE.ShaderMaterial({
+        fragmentShader    : shader.fragmentShader,
+        vertexShader  : shader.vertexShader,
+        uniforms  : shader.uniforms
+    });
+    
+    // build the skybox Mesh 
+    skyboxMesh    = new THREE.Mesh( new THREE.CubeGeometry( 1000, 1000, 1000, 1, 1, 1, null, true ), material );
+    // add it to the scene
+    scene.add( skyboxMesh );*/
+        
 	window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function onWindowResize() {
@@ -225,3 +268,8 @@ function animate() {
 	time = Date.now();
 
 }
+
+
+
+init();
+animate();
